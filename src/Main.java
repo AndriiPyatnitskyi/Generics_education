6ea95e3cb378dec6c80b56e63c404e35acf96d12
Piatnytskyi Andrii <pyatnitskiy.a@gmail.com>
2018-03-18 23:26:50 +0200

    демонтрация того, как валится программа если мы присваиваем массивам более узкий тип, и как дженерики не позволяют это сделать

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        List<Number> numberList= new ArrayList<>();
         List<Integer> integerList = new ArrayList<>();
+        //  Ошибка компиляции
+//        numberList = integerList;
 
-        // Ошибка компиляции. <Integer> теряестся во воремя рантайма
-//    if(integerList instanceof List<Integer>){}
-
-
-        // А вот так норм
-        if (integerList instanceof List) { }
-
+        Number[] numbers = new Number[100];
+        Integer[] integers = new Integer[10];
+        integers[0] = 1;
+        integers[1] = 2;
+        numbers = integers;
+        // Тут программа свалится с ArrayStoreException
+        numbers[1] = 0.01;
+        for (Number number : numbers) {
+            System.out.println(number);
+        }
     }
 }
