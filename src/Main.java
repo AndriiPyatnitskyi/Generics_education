18d50ce96c9e01138f0c2dd21c73d851a4ea2fb5
Piatnytskyi Andrii <pyatnitskiy.a@gmail.com>
2018-03-18 17:23:20 +0200

    Bounds, расширение поведения дженериков

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
 public class Main {
 
     public static void main(String[] args) {
+        One one = new One<>(new Job());
+        one.myMethod();
 
+        //  Так же в качестве Job у нас его подтип SubJob
+        One one1 = new One<>(new SubJob());
+        one1.myMethod();
     }
 
 }
 
-class One{
-    <X> One(X x){
-
+class Job {
+    void printJob() {
+        System.out.println("job");
     }
+}
+
+class SubJob extends Job{
+    void printJob() {
+        System.out.println("sub job");
+    }
+}
+
+//  Расширяем поведение для переменной t,
+//  после установки дженерика <T extends Job> у t появился новый метод
+class One<T extends Job> {
+    One(T t) {
+        this.t = t;
+    }
+
+    private T t;
+
+    public void myMethod() {
+        t.printJob();
+    }
+
 }
 
 
